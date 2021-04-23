a6b083e324e6ca5bc07db804ed556781dc7ca505
Sukarno Sukarno <sukarno.zhang@gmail.com>
2021-04-23 20:45:47 +0800

    Add Flat List

--- App.js
+++ App.js
@@ -6,12 +6,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./BlockRGB";
 
 function HomeScreen() {
- return (
-   <View style={styles.container}>
-     <BlockRGB red={255} green={0} blue={0} />
-     <BlockRGB red={0} green={255} blue={0} />
-     <BlockRGB red={0} green={0} blue={255} />
-   </View>
+   const [colorArray, setColorArray] = useState([
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 0, green: 255, blue: 0, id: "1" },
+    { red: 0, green: 0, blue: 255, id: "2" },
+   ]);
+
+   function renderItem({item}) {
+     return <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+   }
+
+   return (
+     <View style={styles.container}>
+       <Flatlist style ={styles.list} data={colorArray} renderItem={renderItem} />
+     </View>
+   
  );
 }
 
